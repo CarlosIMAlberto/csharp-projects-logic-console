a29bb7afa1186428a98613968f9907ce18520fd9
CarlosIMAlberto <carlosalberto.amic@gmail.com>
2025-10-10 12:18:27 +0100

    Add Project 11.PrimeNumberChecker to the Repository

---
+++ 11.PrimeNumberChecker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace PrimeNumberChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Digite um número inteiro");
+            int valor = CheckerInt(Console.ReadLine());
+            string resultado = PrimeNChecker(valor);
+            System.Console.WriteLine(resultado);
+        }
+        public static int CheckerInt(string valor)
+        {
+            int integer = 0;
+            while (!int.TryParse(valor, out integer))
+            {
+                System.Console.WriteLine("Valor inválido! digite um valor numérico inteiro");
+                valor = Console.ReadLine();
+            }
+            return integer;
+        }
+        public static string PrimeNChecker(int number)
+        {
+            int cont = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    cont++;
+                }
+            }
+            if(cont == 2)
+            {
+                return "Número Primo";
+            }
+            else
+            {
+                return "Este número não é um número primo";
+            }
+        }
+    }
+}
---
+++ 11.PrimeNumberChecker/README.md
@@ -0,0 +1,25 @@
+## Veridicador de Numero Primo
+
+Verificador de número desenvolvido em **C#** onde o utilizador digite um número e app retorna se o número é Primo ou Não.
+
+## 🛠 Tecnologias Utilizadas
+- **Linguagem:** C#
+- **Plataforma:** .NET Console Application
+
+## 📦 Como Executar
+1. Clone este repositório:
+   ```bash
+   git clone https://github.com/CarlosIMAlberto/csharp-projects-logic-console
+Abra o projecto [11.PrimeNumberChecker] no Visual Studio.
+2.Execute o programa (F5).
+
+Digite um Número.
+
+### 📚 Aprendizagens
+
+Separação de Responsabilidades por métodos par uma melhor perfomnce do código.
+Entendimento profundo do uso de Estruturas de Repetição.
+
+📄 Licença
+
+Este projecto está sob a licença MIT — consulte o ficheiro LICENSE para mais informações.
